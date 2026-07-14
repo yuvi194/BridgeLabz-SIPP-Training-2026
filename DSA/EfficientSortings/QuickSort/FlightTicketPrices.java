74da952b64f58a1a833eabd98258d14d8396a34d
yuvika chaudhary <yuvika1904@gmail.com>
2026-07-14 09:50:52 +0530

    [Yuvika] Add: < EfficientSortings question >

---
+++ DSA/EfficientSortings/QuickSort/FlightTicketPrices.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class FlightTicketPrices {
+
+    static void quickSort(int[] arr, int low, int high) {
+
+        if (low < high) {
+
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] <= pivot) {
+
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        for (int i = 0; i < n; i++)
+            prices[i] = sc.nextInt();
+
+        quickSort(prices, 0, n - 1);
+
+        System.out.println("Sorted Ticket Prices:");
+
+        for (int x : prices)
+            System.out.print(x + " ");
+
+        sc.close();
+    }
+}
